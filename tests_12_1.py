14327db04b474ebb42b82ea8ec9652c4a21a58b8
daniilseliverstov <selivrstov91@internet.ru>
2024-09-05 07:40:41 +0300

    "Систематизация и пропуск тестов".

---
+++ tests_12_1.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 10
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        cursor = Runner('Igor')
+        for walk in range(10):
+            cursor.walk()
+        self.assertEqual(cursor.distance, 50)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        cursor = Runner('Vasya')
+        for run in range(10):
+            cursor.run()
+        self.assertEqual(cursor.distance, 100)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        cursor1 = Runner('Petya')
+        cursor2 = Runner('Vanya')
+        for move in range(10):
+            cursor1.walk()
+            cursor2.run()
+        self.assertNotEqual(cursor1.distance, cursor2.distance)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests_12_2.py
@@ -0,0 +1,48 @@
+import unittest
+import runner_and_tournament as rat
+
+
+class TournamentTest(unittest.TestCase):
+    is_frozen = True
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner1 = rat.Runner('Усэйн', 10)
+        self.runner2 = rat.Runner('Андрей', 9)
+        self.runner3 = rat.Runner('Ник', 5)
+
+    @classmethod
+    def tearDownClass(cls):
+        for result in cls.all_results.values():
+            print({key: str(runner) for key, runner in result.items()})
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_1(self):
+        first_run = rat.Tournament(90, self.runner1, self.runner3)
+        result = first_run.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_2(self):
+        second_run = rat.Tournament(90, self.runner2, self.runner3)
+        result = second_run.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_3(self):
+        third_run = rat.Tournament(90, self.runner1, self.runner2, self.runner3)
+        result = third_run.start()
+        last_runner = list(result.values())
+        self.assertTrue(last_runner[-1] == 'Ник')
+        self.all_results[result.values()] = result
+
+
+if __name__ == '__main__':
+    unittest.main()
